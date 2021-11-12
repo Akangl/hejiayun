a7f4d3d4ff707513fd2aa9db155a2cd0087ede20
DESKTOP-IOOC6MM\12551 <1255132438@qq.com>
2021-11-12 22:28:05 +0800

    One

--- src/main/java/com/study/family_service_platform/test/UnitTest.java
+++ src/main/java/com/study/family_service_platform/test/UnitTest.java
@@ -5,11 +5,16 @@ import org.junit.Test;
 /**
  * @author Wenkang.Zhou
  * @date 2021-08-26 22:58
- * @description 测速单元
+ * @description 测试单元
  */
 public class UnitTest {
     @Test
     public void test111() {
         System.out.println("hello");
+    }
+
+    @Test
+    public void testStart() {
+        System.out.println("Staring…………");
     }
 }
